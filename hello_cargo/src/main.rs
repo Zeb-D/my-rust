e6785c696586e4abda9b32d73d289a8d7b02ba5b
Zeb-D <1406721322@qq.com>
2023-04-17 01:12:03 +0800

    +child-process filesystem arg  ffi

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,19 +1,48 @@
-use std::process::Command;
+use std::fmt;
+
+// this extern block links to the libm library
+#[link(name = "m")]
+extern {
+    // this is a foreign function
+    // that computes the square root of a single precision complex number
+    fn csqrtf(z: Complex) -> Complex;
+
+    fn ccosf(z: Complex) -> Complex;
+}
+
+// Since calling foreign functions is considered unsafe,
+// it's common to write safe wrappers around them.
+fn cos(z: Complex) -> Complex {
+    unsafe { ccosf(z) }
+}
 
 fn main() {
-    let output = Command::new("rustc")
-        .arg("--version")
-        .output().unwrap_or_else(|e| {
-            panic!("failed to execute process: {}", e)
-    });
+    // z = -1 + 0i
+    let z = Complex { re: -1., im: 0. };
 
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout);
+    // calling a foreign function is an unsafe operation
+    let z_sqrt = unsafe { csqrtf(z) };
 
-        print!("rustc succeeded and stdout was:\n{}", s);
-    } else {
-        let s = String::from_utf8_lossy(&output.stderr);
+    println!("the square root of {:?} is {:?}", z, z_sqrt);
 
-        print!("rustc failed and stderr was:\n{}", s);
+    // calling safe API wrapped around unsafe operation
+    println!("cos({:?}) = {:?}", z, cos(z));
+}
+
+// Minimal implementation of single precision complex numbers
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct Complex {
+    re: f32,
+    im: f32,
+}
+
+impl fmt::Debug for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.im < 0. {
+            write!(f, "{}-{}i", self.re, -self.im)
+        } else {
+            write!(f, "{}+{}i", self.re, self.im)
+        }
     }
 }
