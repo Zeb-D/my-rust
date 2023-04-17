61c960f28b5c019af2d7ca6efde82a5b2e898637
Zeb-D <1406721322@qq.com>
2023-04-18 01:00:25 +0800

    +testing：unit-test doc-test integration-test dev-dependencies

---
+++ hello_cargo/src/lib.rs
@@ -0,0 +1,15 @@
+// Define this in a crate called `adder`.
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq; // crate for test-only use. Cannot be used in non-test code.
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
---
+++ hello_cargo/tests/interg-lib_test.rs
@@ -0,0 +1,7 @@
+mod common;
+
+#[test]
+fn test_add() {
+    common::setup();
+    // assert_eq!(src::add(3, 2), 5); //todo 这里怎么去调用src/xxx.rsd的代码？？
+}
