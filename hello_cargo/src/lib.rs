ddaacc1f4f35c185b53d348a1614fd29743e5510
Zeb-D <1406721322@qq.com>
2023-04-19 01:31:00 +0800

    +test for src

--- hello_cargo/src/lib.rs
+++ hello_cargo/src/lib.rs
@@ -6,6 +6,14 @@ pub mod adder{
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
     use crate::adder;
