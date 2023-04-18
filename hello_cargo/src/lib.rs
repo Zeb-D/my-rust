ce20c9c195246bf0a98b4c53b6deb8072606c4db
Zeb-D <1406721322@qq.com>
2023-04-19 01:19:22 +0800

    +doc asm doc-test

--- hello_cargo/src/lib.rs
+++ hello_cargo/src/lib.rs
@@ -1,15 +1,18 @@
-// Define this in a crate called `adder`.
-pub fn add(a: i32, b: i32) -> i32 {
+pub mod adder{
+
+    // Define this in a crate called `adder`.
+    pub fn add(a: i32, b: i32) -> i32 {
     a + b
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::adder;
     use pretty_assertions::assert_eq; // crate for test-only use. Cannot be used in non-test code.
 
     #[test]
     fn test_add() {
-        assert_eq!(add(2, 3), 5);
+        assert_eq!(adder::add(2, 3), 5);
     }
 }
--- hello_cargo/tests/interg-lib_test.rs
+++ hello_cargo/tests/interg-lib_test.rs
@@ -1,7 +1,18 @@
 mod common;
 
+
 #[test]
 fn test_add() {
     common::setup();
     // assert_eq!(src::add(3, 2), 5); //todo 这里怎么去调用src/xxx.rsd的代码？？
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    #[test]
+    fn test_add() {
+        assert_eq!(5 , 5);
+    }
 }
