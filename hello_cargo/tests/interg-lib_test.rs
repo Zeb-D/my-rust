77b9ad700e249cf3bf840e631e039553adacb443
Zeb-D <1406721322@qq.com>
2023-04-19 01:31:00 +0800

    +test for src

--- hello_cargo/tests/interg-lib_test.rs
+++ hello_cargo/tests/interg-lib_test.rs
@@ -1,9 +1,9 @@
 mod common;
-
-
 #[test]
 fn test_add() {
     common::setup();
+    println!("1234->{}",hello_cargo::add_two(3));
+    println!("654321->{}",hello_cargo::adder::add(3,9));
     // assert_eq!(src::add(3, 2), 5); //todo 这里怎么去调用src/xxx.rsd的代码？？
     
 }
